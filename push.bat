a8167f99a1730a5e6de4f715408eeafe76f2f402
Z   Jiao <a332574643@gmail.com>
2015-07-08 10:54:51 +0800

    use a new method to push

--- push.bat
+++ push.bat
@@ -3,7 +3,6 @@ taskkill /f /im adb.exe
 adb wait-for-device
 
 :aaa
-adb shell su -c "pkill com.af.synapse"
 adb shell su -c "mount -o remount,rw /"
 adb shell su -c "rm -R /storage/external_SD/res"
 rd /s /q D:\\change
@@ -15,13 +14,20 @@ adb shell su -c "rm /res/synapse/config.json"
 adb shell su -c "rm /res/synapse/config.json.generate*"
 adb shell su -c "rm /res/synapse/actions/*"
 adb shell su -c "rm /res/synapse/files/*"
+adb shell su -c "rm /res/synapse/uci"
 adb shell su -c "cp /storage/external_SD/res/synapse/config.json.generate* /res/synapse/"
 adb shell su -c "chmod 0777 /res/synapse/config.json.generate*"
+adb shell su -c "cp /storage/external_SD/res/synapse/uci /res/synapse/"
+adb shell su -c "chmod 0777 /res/synapse/uci"
 adb shell su -c "cp /storage/external_SD/res/synapse/actions/* /res/synapse/actions/"
 adb shell su -c "chmod 0755 /res/synapse/actions/*"
 adb shell su -c "cp /storage/external_SD/res/synapse/files/* /res/synapse/files/"
 adb shell su -c "chmod 0777 /res/synapse/files/*"
-adb shell su -c "./res/synapse/uci"
-adb shell su -c "am start -a android.intent.action.MAIN -n com.af.synapse/.MainActivity > /dev/null;"
+adb shell su -c "am force-stop com.af.synapse 2> /dev/null"
+adb shell su -c "/res/synapse/uci reset"
+adb shell su -c "/res/synapse/uci"
+ping -n 3 127.0.0.1 > nul
+adb shell su -c "am start -a android.intent.action.MAIN -n com.af.synapse/.MainActivity 2> /dev/null"
+adb shell su -c "rm -R /storage/external_SD/res"
 pause
 goto aaa
